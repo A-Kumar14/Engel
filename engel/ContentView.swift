dc02196a09cd2212011bf92d2c80c193fb766767
A-Kumar14 <arssh.kumar10@gmail.com>
2026-04-21 16:55:34 -0400

    Initial Commit

---
+++ engel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  engel
+//
+//  Created by Arssh Kumar on 4/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ engel/engelApp.swift
@@ -0,0 +1,17 @@
+//
+//  engelApp.swift
+//  engel
+//
+//  Created by Arssh Kumar on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct engelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
